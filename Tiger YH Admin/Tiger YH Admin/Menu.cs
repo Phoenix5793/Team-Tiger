04e277e831e56354e798c2d2a71ada033d0327a4
Johan Svensson <johan@atomicplayboy.net>
2016-04-12 13:52:59 +0200

    Add Menu/Input class

---
+++ Tiger YH Admin/Tiger YH Admin/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tiger_YH_Admin.Models;
+
+namespace Tiger_YH_Admin
+{
+	static class Menu
+	{
+		public static void MainAdminMenu()
+		{
+			Console.WriteLine("Tiger Board!");
+			Console.WriteLine("Admin-meny");
+		}
+
+		public static User LoginMenu(UserStore userStore)
+		{
+			Console.WriteLine("Inloggning");
+
+			Console.Write("Användarnamn: ");
+			string userName = UserInput.LoginGetUserName();
+
+			Console.Write("Lösenord: ");
+			string password = UserInput.LoginGetPassword();
+
+			return userStore.LoginUser(userName, password);
+
+		}
+	}
+}
---
+++ Tiger YH Admin/Tiger YH Admin/UserInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiger_YH_Admin
+{
+	static class UserInput
+	{
+		public static void MainMenuInput()
+		{
+		}
+
+		public static string LoginGetUserName()
+		{
+			return Console.ReadLine();
+
+		}
+
+		public static string LoginGetPassword()
+		{
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.ResetColor();
+			return Console.ReadLine();
+		}
+	}
+}
