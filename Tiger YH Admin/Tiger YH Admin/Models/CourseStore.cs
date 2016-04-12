b454f02ec392ee201391581f74faccf36669049e
Johan Svensson <johan@atomicplayboy.net>
2016-04-12 21:42:07 +0200

    Basic implementation of CourseStore

---
+++ Tiger YH Admin/Tiger YH Admin/Models/CourseStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiger_YH_Admin.Models
+{
+	class CourseStore : DataStore<Course>
+	{
+		public override Course FindById(string id)
+		{
+			//TODO: Skriv tester
+			return DataSet.ToList().SingleOrDefault(c => c.CourseId == id);
+		}
+	}
+}
