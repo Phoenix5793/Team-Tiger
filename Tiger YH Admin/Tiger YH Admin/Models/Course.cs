a73f72288fb892bb5928c08f50e51e422e5115b9
Phoenix5793 <andreascarlen95@hotmail.com>
2016-04-12 11:59:20 +0200

    added course model

---
+++ Tiger YH Admin/Tiger YH Admin/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHelpers;
+
+namespace Tiger_YH_Admin.Models
+{
+    [DelimitedRecord("|")]
+    class Course
+    {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string CourseTeacher { get; set; }
+    }
+}
