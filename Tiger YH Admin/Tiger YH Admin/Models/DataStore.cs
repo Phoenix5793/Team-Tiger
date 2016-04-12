5ca638331fb465779c9f9d68825c1d10931baa80
Johan Svensson <johan@atomicplayboy.net>
2016-04-12 09:31:26 +0200

    Added DataStore scaffold

---
+++ Tiger YH Admin/Tiger YH Admin/Models/DataStore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using FileHelpers;
+
+namespace Tiger_YH_Admin.Models
+{
+	class DataStore<T> where T: class
+	{
+		private readonly string _fileName;
+		private const string FilePrefix = "Data/";
+
+		public IEnumerable<T> DataSet { get; set; }
+
+		public DataStore()
+		{
+			_fileName = FilePrefix + typeof(T).Name + "List.csv";
+			DataSet = Load();
+		}
+
+		private IEnumerable<T> Load()
+		{
+			var csv = new FileHelperEngine<T>();
+			var records = csv.ReadFile(_fileName);
+			return records;
+		}
+
+		public void Save()
+		{
+			var csv = new FileHelperEngine<T>();
+			csv.WriteFile(_fileName, DataSet);
+		}
+	}
+}
