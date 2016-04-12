4ad19d4db0f68267ffab3af6ee46f52cfab4ce86
Johan Svensson <johan@atomicplayboy.net>
2016-04-12 15:57:33 +0200

    Finished admin menu

--- Tiger YH Admin/Tiger YH Admin/Menu.cs
+++ Tiger YH Admin/Tiger YH Admin/Menu.cs
@@ -9,10 +9,18 @@ namespace Tiger_YH_Admin
 {
 	static class Menu
 	{
-		public static void MainAdminMenu()
+		public static int MainAdminMenu()
 		{
 			Console.WriteLine("Tiger Board!");
 			Console.WriteLine("Admin-meny");
+			Console.WriteLine();
+			Console.WriteLine("1. Skapa användare");
+			Console.WriteLine("2. Sök efter användare");
+			Console.WriteLine("3. Skapa kurs");
+			Console.WriteLine("4. Skapa klass");
+			Console.WriteLine();
+			Console.Write("Ditt val: ");
+			return UserInput.GetInput<int>();
 		}
 
 		public static User LoginMenu(UserStore userStore)
