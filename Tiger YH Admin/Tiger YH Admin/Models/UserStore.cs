a31a16c86d8e9d4f32d762198cceea60ec002a40
Johan Svensson <johan@atomicplayboy.net>
2016-04-12 21:24:33 +0200

    Tabs to space

--- Tiger YH Admin/Tiger YH Admin/Models/UserStore.cs
+++ Tiger YH Admin/Tiger YH Admin/Models/UserStore.cs
@@ -9,47 +9,47 @@ namespace Tiger_YH_Admin.Models
 {
     class UserStore : DataStore<User>
     {
-	    public override User FindById(string id)
-	    {
-			//TODO: Skriv tester
-		    return DataSet.ToList().SingleOrDefault(u => u.UserName == id);
-	    }
+      public override User FindById(string id)
+      {
+      //TODO: Skriv tester
+        return DataSet.ToList().SingleOrDefault(u => u.UserName == id);
+      }
 
-	    public User GetUserByName(string name)
+      public User GetUserByName(string name)
         {
-			//TODO: Skriv tester
-	        List<User> userList = Load().ToList();
+      //TODO: Skriv tester
+          List<User> userList = Load().ToList();
 
-	        return userList.SingleOrDefault(u => u.UserName == name);
+          return userList.SingleOrDefault(u => u.UserName == name);
         }
 
-	    public User LoginUser(string name, string password)
-	    {
-			//TODO: Skriv tester
-		    User user = GetUserByName(name);
+      public User LoginUser(string name, string password)
+      {
+      //TODO: Skriv tester
+        User user = GetUserByName(name);
 
-		    if (user != null && password == user.Password)
-		    {
-			    return user;
-		    }
-		    return null;
-	    }
+        if (user != null && password == user.Password)
+        {
+          return user;
+        }
+        return null;
+      }
 
-	    public bool AddUser(User newUser)
-	    {
-			// TODO: Skriv tester
-		    User existingUser = DataSet.SingleOrDefault(u => u.UserName == newUser.UserName);
-		    List<User> userList = DataSet.ToList();
+      public bool AddUser(User newUser)
+      {
+      // TODO: Skriv tester
+        User existingUser = DataSet.SingleOrDefault(u => u.UserName == newUser.UserName);
+        List<User> userList = DataSet.ToList();
 
-		    if (existingUser == null)
-		    {
-			    userList.Add(newUser);
-			    DataSet = userList;
-				Save();
-			    return true;
-		    }
+        if (existingUser == null)
+        {
+          userList.Add(newUser);
+          DataSet = userList;
+        Save();
+          return true;
+        }
 
-		    return false;
-	    }
+        return false;
+      }
     }
 }
