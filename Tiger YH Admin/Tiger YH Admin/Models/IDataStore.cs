dec905dccaf2be696adeecefd757d7bef1982c71
Johan Svensson <johan@atomicplayboy.net>
2016-04-14 10:03:08 +0200

    Add DataStore prop to interface

--- Tiger YH Admin/Tiger YH Admin/Models/IDataStore.cs
+++ Tiger YH Admin/Tiger YH Admin/Models/IDataStore.cs
@@ -4,6 +4,7 @@ namespace Tiger_YH_Admin.Models
 {
 	public interface IDataStore<T> where T: class
 	{
+		IEnumerable<T> DataSet { get; set; }
 		IEnumerable<T> Load();
 		void Save();
 		T FindById(string id);
