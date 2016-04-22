042b2ca92ec781e3fb90f0a552e41d239592b429
Johan Svensson <johan@atomicplayboy.net>
2016-04-22 12:32:31 +0200

    Untabify

--- Tiger YH Admin/Tiger YH Admin/Program.cs
+++ Tiger YH Admin/Tiger YH Admin/Program.cs
@@ -14,63 +14,63 @@ namespace Tiger_YH_Admin
     {
         static void Main(string[] args)
         {
-	        bool loopMenu = true;
+            bool loopMenu = true;
 
-	        User user;
-			UserStore userStore = new UserStore();
+            User user;
+            UserStore userStore = new UserStore();
 
-			do
-			{
-				Console.Clear();
-				string[] credentials = Menu.LoginMenu();
-				user = userStore.LoginUser(credentials[0], credentials[1]);
+            do
+            {
+                Console.Clear();
+                string[] credentials = Menu.LoginMenu();
+                user = userStore.LoginUser(credentials[0], credentials[1]);
 
-				if (user != null)
-				{
-					Console.WriteLine("Inloggad som " + user.UserLevel);
-					loopMenu = false;
-				}
-				else
-				{
-					Console.WriteLine("Fel användarnamn eller lösenord");
-					Console.ReadKey();
-				}
+                if (user != null)
+                {
+                    Console.WriteLine("Inloggad som " + user.UserLevel);
+                    loopMenu = false;
+                }
+                else
+                {
+                    Console.WriteLine("Fel användarnamn eller lösenord");
+                    Console.ReadKey();
+                }
 
-			} while (loopMenu);
+            } while (loopMenu);
 
-	        loopMenu = true;
-	        while (loopMenu)
-	        {
-		        if (user.UserLevel == UserLevel.Admin)
-		        {
-			        Console.Clear();
-                    
-			        int menuChoice = Menu.MainAdminMenu();
+            loopMenu = true;
+            while (loopMenu)
+            {
+                if (user.UserLevel == UserLevel.Admin)
+                {
+                    Console.Clear();
 
-			        switch (menuChoice)
-			        {
-						case 1:
-					        UserCreator creator = new UserCreator();
-					        creator.Create(userStore);
-					        break;
-						case 2:
-					        UserManagerPresenter.Run();
-					        break;
-						case 3:
-					        Console.WriteLine("Ej implementerad");
-					        break;
-						case 4:
-							var ds = new EducationClassStore();
-							EducationClassCreator edCreator = new EducationClassCreator();
-					        edCreator.Create(ds);
-					        break;
-						case 5:
-					        ClassListPresenter.Run();
-					        break;
-			        }
+                    int menuChoice = Menu.MainAdminMenu();
 
-			        Console.ReadKey();
-		        }
+                    switch (menuChoice)
+                    {
+                        case 1:
+                            UserCreator creator = new UserCreator();
+                            creator.Create(userStore);
+                            break;
+                        case 2:
+                            UserManagerPresenter.Run();
+                            break;
+                        case 3:
+                            Console.WriteLine("Ej implementerad");
+                            break;
+                        case 4:
+                            var ds = new EducationClassStore();
+                            EducationClassCreator edCreator = new EducationClassCreator();
+                            edCreator.Create(ds);
+                            break;
+                        case 5:
+                            ClassListPresenter.Run();
+                            break;
+                    }
+
+                    Console.ReadKey();
+                }
                 if(user.UserLevel == UserLevel.EducationSupervisor)
                 {
                     Console.Clear();
@@ -88,11 +88,11 @@ namespace Tiger_YH_Admin
                             break;
                         case 3:
                             Menu.ManageCourses();
-                            
+
                             break;
                         case 4:
                             Menu.ManageStudents();
-                          
+
                             break;
                        case 5:
                             var ds = new EducationClassStore();
@@ -102,17 +102,13 @@ namespace Tiger_YH_Admin
                     }
 
                     Console.ReadKey();
-
-
                 }
-
-
                 else
-		        {
-			        Console.WriteLine("Du har inte admin-rättigheter");
-			        Console.ReadKey();
-		        }
-	        }
-		}
+                {
+                    Console.WriteLine("Du har inte admin-rättigheter");
+                    Console.ReadKey();
+                }
+            }
+        }
     }
 }
