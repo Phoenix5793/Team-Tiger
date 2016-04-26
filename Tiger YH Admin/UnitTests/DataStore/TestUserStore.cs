f01ca3f714666b717a3090127c1f95e4e7c86e13
Johan Svensson <johan@atomicplayboy.net>
2016-04-26 12:08:44 +0200

    Test if admin account exists

---
+++ Tiger YH Admin/UnitTests/DataStore/TestUserStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tiger_YH_Admin;
+using Tiger_YH_Admin.Models;
+
+namespace UnitTests.DataStore
+{
+    [TestClass]
+    public class TestUserStore
+    {
+        [TestMethod]
+        public void Admin_Account_Exists()
+        {
+            UserStore userStore = new UserStore();
+            User expectedUser = userStore.FindById("admin");
+
+            Assert.IsTrue(expectedUser.UserName == "admin");
+        }
+    }
+}
