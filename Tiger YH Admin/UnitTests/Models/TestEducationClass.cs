03725487a4ba8a53d07b72d362048f9e096eb322
Johan Svensson <johan@atomicplayboy.net>
2016-04-26 21:37:13 +0200

    Test EducationClass

---
+++ Tiger YH Admin/UnitTests/Models/TestEducationClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tiger_YH_Admin;
+using Tiger_YH_Admin.Models;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class TestEducationClass
+    {
+        private EducationClass _testClass;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _testClass = new EducationClass()
+            {
+                ClassId = "testclass",
+                Description = "The Joy of Painting with Bob Ross",
+                EducationSupervisorId = "bobross",
+                StudentString = "adam,bertil,caesar,david,erik,johndoe"
+            };
+        }
+
+        [TestMethod]
+        public void Has_Class_Id()
+        {
+            string expected = "testclass";
+            string actual = _testClass.ClassId;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Returns_Student_String()
+        {
+            string expected = "adam,bertil,caesar,david,erik,johndoe";
+            string actual = _testClass.StudentString;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Returns_Student_List()
+        {
+            List<string> expected = new List<string>() {"adam", "bertil", "caesar", "david", "erik", "johndoe"};
+            List<string> actual = _testClass.GetStudentList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Has_Student_String()
+        {
+            string input = "david";
+
+            bool expected = true;
+            bool actual = _testClass.HasStudent(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void HasStudent_Validates_Properly()
+        {
+            string input = "john";
+
+            bool expected = false;
+            bool actual = _testClass.HasStudent(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
