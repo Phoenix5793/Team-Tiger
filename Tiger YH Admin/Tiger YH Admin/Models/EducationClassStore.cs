b631bfb0db58ced47371d394ee60647d0121cdd9
Johan Svensson <johan@atomicplayboy.net>
2016-04-27 10:57:04 +0200

    Tests for EducationClassStore

--- Tiger YH Admin/Tiger YH Admin/Models/EducationClassStore.cs
+++ Tiger YH Admin/Tiger YH Admin/Models/EducationClassStore.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Tiger_YH_Admin.Models
 {
-    class EducationClassStore : DataStore<EducationClass>
+    public class EducationClassStore : DataStore<EducationClass>
     {
         public override EducationClass FindById(string id)
         {
---
+++ Tiger YH Admin/UnitTests/DataStore/TestEducationClassStore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tiger_YH_Admin.Models;
+
+namespace UnitTests.DataStore
+{
+    [TestClass]
+    public class TestEducationClassStore
+    {
+        private EducationClassStore _classStore;
+        private EducationClass _testClass;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _testClass = new EducationClass
+            {
+                ClassId = "su15",
+                Description = "Systemutvecklare, agil applikationsprogrammering",
+                EducationSupervisorId = "tina"
+            };
+
+            List<EducationClass> classList = new List<EducationClass> {_testClass};
+
+            _classStore = new EducationClassStore();
+            _classStore.DataSet = classList;
+        }
+
+        [TestMethod]
+        public void FindById__Can_Find_Class()
+        {
+            string input = "su15";
+            string expected = "su15";
+
+            EducationClass foundClass = _classStore.FindById(input);
+            string actual = foundClass.ClassId;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindById__Returns_Null_If_Class_Not_Found()
+        {
+            EducationClass foundClass = _classStore.FindById("qwerty");
+
+            Assert.IsNull(foundClass);
+        }
+
+    }
+}
