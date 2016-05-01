dc732cff98d04ca9a599787c152cafffb35b4656
Johan Svensson <johan@atomicplayboy.net>
2016-05-01 14:14:55 +0200

    Add validation class, validate email address

---
+++ Tiger YH Admin/Tiger YH Admin/Validation.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Tiger_YH_Admin
+{
+    public static class Validation
+    {
+        public static bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email, $".*@[a-z0-9-]+[.][a-z]+");
+        }
+    }
+}
---
+++ Tiger YH Admin/UnitTests/TestValidation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tiger_YH_Admin;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestValidation
+    {
+        [TestMethod]
+        public void IsValidEmail__Typical_Address()
+        {
+            string input = "foo@example.net";
+            bool expected = true;
+
+            bool actual = Validation.IsValidEmail(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsValidEmail__Has_Periods()
+        {
+            string input = "foo.bar@example.net";
+            bool expected = true;
+
+            bool actual = Validation.IsValidEmail(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsValidEmail__Has_Plus()
+        {
+            string input = "foo+spamtrap@example.net";
+            bool expected = true;
+
+            bool actual = Validation.IsValidEmail(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsValidEmail__Has_Periods_And_Plus()
+        {
+            string input = "foo.bar+spamtrap@example.net";
+            bool expected = true;
+
+            bool actual = Validation.IsValidEmail(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
