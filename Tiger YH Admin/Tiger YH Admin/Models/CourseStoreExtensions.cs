5fb06835fcb5bd6dcdae58fd4893f39151c7a779
Johan Svensson <johan@atomicplayboy.net>
2016-05-03 14:58:58 +0200

    Add course store IEnumerable extensions

---
+++ Tiger YH Admin/Tiger YH Admin/Models/CourseStoreExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiger_YH_Admin.Models
+{
+    static class CourseStoreExtensions
+    {
+        public static IEnumerable<Course> Unmanned(this IEnumerable<Course> courses)
+        {
+            return courses.Where(c => c.CourseTeacher == string.Empty);
+        }
+
+        public static IEnumerable<Course> Manned(this IEnumerable<Course> courses)
+        {
+            return courses.Where(c => c.CourseTeacher != string.Empty);
+        }
+
+        public static IEnumerable<Course> Finished(this IEnumerable<Course> courses)
+        {
+            return courses.Where(c => c.EndDate < DateTime.Today);
+        }
+
+        public static IEnumerable<Course> Current(this IEnumerable<Course> courses)
+        {
+            return courses.Where(c => c.StartDate < DateTime.Today && c.EndDate > DateTime.Today);
+        }
+
+        public static IEnumerable<Course> Future(this IEnumerable<Course> courses)
+        {
+            return courses.Where(c => c.StartDate > DateTime.Today);
+        }
+    }
+}
