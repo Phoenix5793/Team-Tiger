7cd29998eb56f182de26ac7d8f404d42a5e908a9
Phoenix5793 <andreascarlen95@hotmail.com>
2016-05-10 12:57:27 +0200

    added Luhn Method

--- Tiger YH Admin/Tiger YH Admin/UserInput.cs
+++ Tiger YH Admin/Tiger YH Admin/UserInput.cs
@@ -11,7 +11,7 @@ namespace Tiger_YH_Admin
         public static T GetInput<T>()
         {
             string input = Console.ReadLine();
-            return (T) Convert.ChangeType(input, typeof (T));
+            return (T)Convert.ChangeType(input, typeof(T));
         }
 
         public static T GetInput<T>(string message)
@@ -88,6 +88,21 @@ namespace Tiger_YH_Admin
             {
                 key = Console.ReadKey().Key;
             } while (key != ConsoleKey.Enter);
+        }
+
+        private static bool IsValidLuhn(this string number)
+        {
+            int[] DELTAS = new int[] { 0, 1, 2, 3, 4, -4, -3, -2, -1, 0 };
+            int checksum = 0;
+            char[] chars = number.ToCharArray();
+            for (int i = chars.Length - 1; i > -1; i--)
+            {
+                int j = ((int)chars[i]) - 48;
+                checksum += j;
+                if (((i - chars.Length) % 2) == 0)
+                    checksum += DELTAS[j];
+            }
+             return ((checksum % 10) == 0);
         }
     }
 }
