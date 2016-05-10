853437f7cf6a4d0cde4bc8daf54736f08e93067a
Johan Svensson <johan@atomicplayboy.net>
2016-05-10 13:38:17 +0200

    Cleanup

--- Tiger YH Admin/Tiger YH Admin/Validation.cs
+++ Tiger YH Admin/Tiger YH Admin/Validation.cs
@@ -11,17 +11,17 @@ namespace Tiger_YH_Admin
 
         public static bool IsValidLuhn(this string number)
         {
-            int[] DELTAS = new int[] { 0, 1, 2, 3, 4, -4, -3, -2, -1, 0 };
+            int[] deltas = { 0, 1, 2, 3, 4, -4, -3, -2, -1, 0 };
             int checksum = 0;
             char[] chars = number.ToCharArray();
             for (int i = chars.Length - 1; i > -1; i--)
             {
                 int j = ((int)chars[i]) - 48;
                 checksum += j;
-                if (((i - chars.Length) % 2) == 0)
-                    checksum += DELTAS[j];
+                if ((i - chars.Length) % 2 == 0)
+                    checksum += deltas[j];
             }
-            return ((checksum % 10) == 0);
+            return (checksum % 10) == 0;
         }
     }
 }
