c1c038baffd2d3b52853d3c15665a46d1bc69bd7
Johan Svensson <johan@atomicplayboy.net>
2016-05-11 13:59:08 +0200

    Admin account check/creation

--- Tiger YH Admin/Tiger YH Admin/Program.cs
+++ Tiger YH Admin/Tiger YH Admin/Program.cs
@@ -1,4 +1,5 @@
-﻿using Tiger_YH_Admin.DataStore;
+﻿using System;
+using Tiger_YH_Admin.DataStore;
 using Tiger_YH_Admin.Models;
 using Tiger_YH_Admin.Presenters;
 
@@ -12,9 +13,40 @@ namespace Tiger_YH_Admin
             while (true)
             {
                 UserStore userStore = new UserStore();
+                ProgramSetup(userStore);
                 User user = UserManagerPresenter.LoginUser(userStore);
                 Menu.MainMenu(user);
             }
+        }
+
+        static void ProgramSetup(UserStore userStore)
+        {
+            User adminUser = userStore.FindById("admin");
+
+            if (adminUser == null)
+            {
+                Console.WriteLine("Admin-konto existerar ej! Skapar...");
+                CreateAdmin(userStore);
+                Console.WriteLine("Admin-konto skapat.");
+                Console.WriteLine("Användarnamn: admin");
+                Console.WriteLine("Lösenord: admin");
+                Console.WriteLine("Byt genast lösenord på kontot!");
+                UserInput.WaitForContinue();
+            }
+        }
+
+        static void CreateAdmin(UserStore userStore)
+        {
+            User admin = new User
+            {
+                UserName = "admin",
+                FirstName = "Admin",
+                Password = "admin",
+                UserLevel = UserLevel.Admin
+            };
+
+            userStore.AddItem(admin);
+            userStore.Save();
         }
     }
 }
