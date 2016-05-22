bc39c30d3a82be5c60b860e4463dc31c154d718f
Johan Svensson <johan@atomicplayboy.net>
2016-05-22 16:00:11 +0200

    Test GradeId

--- Tiger YH Admin/UnitTests/Models/TestGrade.cs
+++ Tiger YH Admin/UnitTests/Models/TestGrade.cs
@@ -8,6 +8,28 @@ namespace UnitTests.Models
     [TestClass]
     public class TestGrade
     {
+        private Grade _testGrade;
+        private Grade _testGradeGoal;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _testGrade = new Grade
+            {
+                CourseId = "kurs",
+                StudentId = "user",
+                Result = GradeLevel.G
+            };
+
+            _testGradeGoal = new Grade
+            {
+                CourseId = "kurs",
+                StudentId = "user",
+                CourseGoal = "1",
+                Result = GradeLevel.VG
+            };
+        }
+
         [TestMethod]
         public void Grade__Has_Default_Grade()
         {
@@ -15,6 +37,28 @@ namespace UnitTests.Models
             GradeLevel expected = GradeLevel.IG;
 
             GradeLevel actual = input.Result;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Grade__Course_Grade_Has_Valid_GradeId()
+        {
+            Grade input = _testGrade;
+            string expected = "kurs:user";
+
+            string actual = input.GradeId;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Grade__Goal_Grade_Has_Valid_GradeId()
+        {
+            Grade input = _testGradeGoal;
+            string expected = "kurs:user:1";
+
+            string actual = input.GradeId;
 
             Assert.AreEqual(expected, actual);
         }
